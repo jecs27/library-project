1ea9c0370ff97ca38b929aa56d94b5dc0955f846
Jesus Calderon <jesus.calderon2708@gmail.com>
2022-04-12 17:57:58 -0600

    se agrega uso de service en controller

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,13 +1,15 @@
 import { Body, Controller,Get, Post, HttpCode, Delete, Param, Put } from '@nestjs/common';;
 import { CreateBook } from './dto/create-book.dto';
-import { Request, Response } from 'express';
+import { BooksService } from './books.service';
 
 @Controller('books')
 export class BooksController {
 
+  constructor(private booksService: BooksService){}
+  
   @Get()
-  getBooks(): string {
-    return 'Controller Books';
+  getBooks(): {} {
+    return this.booksService.getBooks();
   }
 
   @Post()
